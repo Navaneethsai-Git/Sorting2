d232632f7d958935196392c8e559dd3cf9fd7e58
Lucifer <Lucifer@10.12.26.48>
2022-09-19 22:40:53 +0530

    Bubble Sort

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,9 +1,25 @@
-
 public class BubbleSort {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int arr[] = {4,24,65,3,34,58,83,67,6,44,23,54,65};
+		System.out.println("Array Before Bubble Sort");  
+        for(int i=0; i < arr.length; i++){  
+                System.out.print(arr[i] + " ");  
+        }  
+        System.out.println();
+		int n = arr.length;  
+	    int t = 0;
+	    for(int i=0; i < n; i++){
+	    	for(int j=1; j < (n-i); j++){
+	    		if(arr[j-1] > arr[j]){
+	    			t = arr[j-1];  
+	    			arr[j-1] = arr[j];  
+	    			arr[j] = t;
+	    System.out.println("Bubble Sorting");
+	    for(int h=0; h < arr.length; h++){  
+	    	System.out.print(arr[h] + " ");
+	    }
+	    		}
+	    	}
+	    }
 	}
-
 }
